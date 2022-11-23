fa5113c3b85372da851342d7c31332367f6fbcb9
rfgoo <rfgoo@iscte-iul.pt>
2022-11-23 16:08:19 +0000

    Initial commit

---
+++ redes-oticas.py
@@ -0,0 +1,58 @@
+#pip install dijkstra
+from dijkstra import DijkstraSPF, Graph
+
+#definição do graph
+"""
+adjacency_list = {1: (2, 6),
+                  2: (1, 3, 6),
+                  3: (2, 4, 5),
+                  4: (3, 5),
+                  5: (3, 4, 6),
+                  6: (1, 2, 5)
+                  }
+
+
+
+edge_weights = {(1, 2): 500,
+                (1, 6): 800,
+                (2, 1): 500,
+                (2, 3): 500,
+                (2, 6): 300,
+                (3, 2): 500,
+                (3, 4): 500,
+                (3, 5): 300,
+                (4, 3): 500,
+                (4, 5): 800,
+                (5, 3): 300,
+                (5, 4): 800,
+                (5, 5): 500,
+                (6, 1): 800,
+                (6, 2): 300,
+                (6, 5): 500
+                }
+graph = Graph(adjacency_list, edge_weights)
+dijkstra = DijkstraSPF(graph, 1)
+print(dijkstra.get_path(4))
+print(dijkstra.get_distance(4))
+"""
+
+length = [500, 800, 500, 500, 300, 500, 500, 300, 500, 800, 300, 800, 500, 800, 300, 500]
+adjacency_list = {1: (2, 6),
+                  2: (1, 3, 6),
+                  3: (2, 4, 5),
+                  4: (3, 5),
+                  5: (3, 4, 6),
+                  6: (1, 2, 5)
+                  }
+
+combs = []
+for x, y in adjacency_list.items():
+    for z in y:
+        combs.append((x, z))
+
+edge_weights = dict(zip(combs, length))
+
+graph = Graph(adjacency_list, edge_weights)
+dijkstra = DijkstraSPF(graph, 1)
+print(dijkstra.get_path(4))
+print(dijkstra.get_distance(4))
