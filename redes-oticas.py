4c409baeccd33e7832d750b872a8e3eecea2cdf7
rfgoo <rfgoo@iscte-iul.pt>
2022-11-23 16:19:54 +0000

    comentários

--- redes-oticas.py
+++ redes-oticas.py
@@ -36,7 +36,10 @@ print(dijkstra.get_path(4))
 print(dijkstra.get_distance(4))
 """
 
-length = [500, 800, 500, 500, 300, 500, 500, 300, 500, 800, 300, 800, 500, 800, 300, 500]
+"""
+dicionário:
+nó atual: lista com os nos adjacentes
+"""
 adjacency_list = {1: (2, 6),
                   2: (1, 3, 6),
                   3: (2, 4, 5),
@@ -44,6 +47,10 @@ adjacency_list = {1: (2, 6),
                   5: (3, 4, 6),
                   6: (1, 2, 5)
                   }
+"""
+custo de cada link pela ordem do dicionário acima
+"""
+length = [500, 800, 500, 500, 300, 500, 500, 300, 500, 800, 300, 800, 500, 800, 300, 500]
 
 combs = []
 for x, y in adjacency_list.items():
